9e76033a7b70f8f058d7f63ddddf73684e92ce8f
Stefan Isak <isakhandy@gmail.com>
2022-04-16 17:41:53 +0200

    copyright notice

--- include/clapp.hpp
+++ include/clapp.hpp
@@ -1,3 +1,32 @@
+/*
+   ___ _      _   ___ ___ 
+  / __| |    /_\ | _ \ _ \  Command Line Argument Parser++
+ | (__| |__ / _ \|  _/  _/  Version 1.0.0
+  \___|____/_/ \_\_| |_|    https://github.com/sisakat/clapp
+                            
+Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+SPDX-License-Identifier: MIT
+Copyright (c) 2022 Stefan Isak <http://sisak.at>.
+
+Permission is hereby  granted, free of charge, to any  person obtaining a copy
+of this software and associated  documentation files (the "Software"), to deal
+in the Software  without restriction, including without  limitation the rights
+to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <algorithm>
 #include <cassert>
 #include <functional>
@@ -11,6 +40,8 @@
 
 namespace clapp
 {
+
+
 
 /* Type conversions */
 
